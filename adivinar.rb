0bd975e61821442de69a56bb514d6c0d189b6ae4
bian2510 <35665705+bian2510@users.noreply.github.com>
2018-05-18 23:19:07 -0500

    Add files via upload

---
+++ adivinar.rb
@@ -0,0 +1,12 @@
+	$rand = rand(1..10)
+GANADOR = $rand
+def adivinar
+	puts "Adivina el numero del 1 al 10"
+	@numero = gets.chomp.to_i
+	while @numero != GANADOR
+		puts "Error intenta de nuevo, ingresa un nuevo numero"
+		@numero = gets.chomp.to_i
+	end
+	puts "Correcto! el numero es #{GANADOR}"
+end
+adivinar
